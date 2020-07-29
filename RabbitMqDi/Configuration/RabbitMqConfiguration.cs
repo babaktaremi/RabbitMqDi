4c20d436763838f2a9db63f28be1c663d5d28598
babaktaremi <babaktaremi@yahoo.com>
2020-07-30 02:23:18 +0430

    Add project files.

---
+++ RabbitMqDi/Configuration/RabbitMqConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqDi.Configuration
+{
+    public static class RabbitMqExtensions
+    {
+        public static void AddRabbitMq(this IServiceCollection services, string url)
+        {
+            services.Configure<RabbitMqSetting>(r => r.RabbitMqUrl = url);
+            services.AddTransient<RabbitMqService>();
+        }
+    }
+
+
+    public class RabbitMqSetting
+    {
+        public string RabbitMqUrl { get; set; }
+    }
+
+    public class RabbitMqService
+    {
+        private RabbitMqSetting _settings;
+        public IModel Channel { get; private set; }
+        public RabbitMqService(IOptions<RabbitMqSetting> options)
+        {
+            _settings = options.Value;
+
+            var factory = new ConnectionFactory() { HostName = _settings.RabbitMqUrl };
+            var connection = factory.CreateConnection();
+             Channel = connection.CreateModel();
+            // Consumer= new EventingBasicConsumer(this.Channel);
+        }
+
+        public AsyncEventingBasicConsumer CreateConsumerAsync()
+        {
+            return new AsyncEventingBasicConsumer(Channel);
+        }
+
+        public EventingBasicConsumer CreateConsumer()
+        {
+            return new EventingBasicConsumer(Channel);
+        }
+    }
+}
---
+++ RabbitMqDi/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMqDi.Configuration;
+
+namespace RabbitMqDi.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly RabbitMqService _rabbitMq;
+
+        public IndexModel(ILogger<IndexModel> logger, RabbitMqService rabbitMq)
+        {
+            _logger = logger;
+            _rabbitMq = rabbitMq;
+        }
+
+        public void OnGet([FromServices] RabbitMqService rabbitMq)
+        {
+            rabbitMq.Channel.QueueDeclare(queue: "TestQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var message = Encoding.UTF8.GetBytes("This is Working!!!!");
+            rabbitMq.Channel.BasicPublish("", routingKey: "TestQueue", basicProperties: null, body: message);
+        }
+
+
+        public void OnGetLogMessages()
+        {
+            var result = "";
+            _rabbitMq.Channel.QueueDeclare(queue: "TestQueue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            var consumer = _rabbitMq.CreateConsumer();
+            consumer.Received += ConsumerOnReceived;
+            _rabbitMq.Channel.BasicConsume(queue: "TestQueue", autoAck: true, consumer: consumer);
+        }
+
+        private void ConsumerOnReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body.ToArray());
+            _logger.LogWarning("New Message "+message);
+        }
+    }
+}
